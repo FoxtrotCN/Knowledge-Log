1b7590473b231307061570ea6b92ead7df11c0a2
Fernando <fernandostarlin1012@gmail.com>
2024-08-13 16:02:45 -0400

    Enable the actions for register new entries for the topics as well as edit these entries

--- core_app/urls.py
+++ core_app/urls.py
@@ -9,5 +9,13 @@ urlpatterns = [
     # Topics page
     path('topics/', views.get_topics, name='topics'),
     # Detail page for a single topic
-    path('topics/<int:topic_id>/', views.get_topic, name='topic')
+    path('topics/<int:topic_id>/', views.get_topic, name='topic'),
+    # Page for adding a new topic
+    path('new_topic/', views.add_new_topic, name='new_topic'),
+    # Page for adding a new entry
+    path('new_entry/<int:topic_id>/', views.add_new_entry, name='new_entry'),
+    # Page for editing an entry
+    path('edit_entry/<int:entry_id>', views.edit_entry, name='edit_entry'),
+
+
 ]
