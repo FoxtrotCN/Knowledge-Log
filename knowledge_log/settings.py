1f28e8b621ff0c93ef6e9974c8126c24f80a4a75
Fernando <fernandostarlin1012@gmail.com>
2024-08-15 12:55:54 -0400

    Ready for deployment

--- knowledge_log/settings.py
+++ knowledge_log/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
+import secrets
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+3oxh@b!--e+$n@b70+1m@d55(ius(!3axs@ngrw+j^v6nhht='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
+
 
 ALLOWED_HOSTS = []
 
@@ -133,3 +139,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Knowledge Log settings
 LOGIN_URL = 'users:login'
+
+# Heroku settings
+
